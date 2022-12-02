53316d60bc1d3b9589b1c9b7834172720241eebe
thibben <oskar.thibblin@gmail.com>
2022-12-02 12:01:39 +0100

    ugly but short

--- day02/index.js
+++ day02/index.js
@@ -7,21 +7,14 @@ const readInputFile = (input = "input.txt") => {
 const getSolutionPart1 = () => {
     const points = new Map([ ["A Y", 8], ["B Z", 9], ["C X", 7], ["A X", 4], ["B Y", 5], ["C Z", 6], ["A Z", 3], ["B X", 1], ["C Y", 2] ]);
     const game = readInputFile();
-    let total = 0;
-    for(const x of game){
-        total += points.get(x);
-    }
-    return total;
+    return game.map(x => points.get(x)).reduce((a,b) => a+b);
 }
 
 const getSolutionPart2 = () => {
     const translated = new Map([ ["A Y", 4], ["B Z", 9], ["C X", 2], ["A X", 3], ["B Y", 5], ["C Z", 7], ["A Z", 8], ["B X", 1], ["C Y", 6] ]);
     const game = readInputFile();
-    let total = 0;
-    for(const x of game){
-        total += translated.get(x);
-    }
-    return total;
+    return game.map(x => translated.get(x)).reduce((a,b) => a+b);
+    
 }
 
 console.log("JavaScript");
